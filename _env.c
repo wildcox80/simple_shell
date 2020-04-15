c067edfdb0259a7f0cf3131aa1cbe69df87373ea
Wilds Rincon <1588@holbertonschool.com>
2020-04-14 23:19:33 -0500

    Adding changes in folder and adding file _env.c

--- _env.c
+++ _env.c
@@ -32,3 +32,28 @@ char *_getenv(const char *name)
 	/* no match */
 	return (NULL);
 }
+
+
+/**
+ * printenv - prints the environment of a process
+ *
+ * Return: void
+ */
+void printenv(void)
+{
+	unsigned int i, j;
+
+	i = 0;
+	while (environ[i] != NULL)
+	{
+		j = 0;
+		while (environ[i][j] != '\0')
+		{
+			_putchar(environ[i][j]);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
+
